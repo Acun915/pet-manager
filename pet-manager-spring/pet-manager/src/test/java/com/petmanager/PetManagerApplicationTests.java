8523e30f2a8adbf1c973b46e26ccc788227625b2
Przemyslaw <przemoacun@gmail.com>
2021-01-09 12:03:21 +0100

    initial commit'

---
+++ pet-manager-spring/pet-manager/src/test/java/com/petmanager/PetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
