05e55a468bc291cfc38835c935a5e5c19e8d03a1
Przemyslaw <przemoacun@gmail.com>
2021-01-14 12:43:12 +0100

    added navigation links to header

--- pet-manager-frontend/pet-manager/src/components/header/HeaderComponent.jsx
+++ pet-manager-frontend/pet-manager/src/components/header/HeaderComponent.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./HeaderComponent.css";
 
 const HeaderComponent = () => {
-  return <div></div>;
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="nav-item">
+          <Link to="/">Pet-Manager</Link>
+        </div>
+
+        <div className="nav-item">
+          <Link to="/pets">Your Pets</Link>
+        </div>
+
+        <div className="nav-item">
+          <Link to="/dailyfeeding">Daily Feeding</Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default HeaderComponent;
